967c0a28774fb9a8565c4f778eb9e1f37ad59e7c
Super89123 <tedswa@mail.ru>
2024-01-17 06:48:34 +0400

    Modification

--- src/main/java/org/super89/supermegamod/learningplugin/EventS.java
+++ src/main/java/org/super89/supermegamod/learningplugin/EventS.java
@@ -13,40 +13,45 @@ import java.util.HashMap;
 
 public class EventS implements Listener {
     GUI gui = new GUI();
+    public HashMap playerSwitches = gui.playerSwitches;
+
 
     public ArmorStand armorStand;
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event){
 
 
-        if(event.getPlayer().hasPermission("pet.self")) {
+
 
             Player player = event.getPlayer();
             String s = player.getDisplayName();
+            boolean b1 = (boolean) playerSwitches.get(player);
+            if(!b1) {
 
 
-            if (armorStand == null) {
-                World world = player.getWorld();
+                if (armorStand == null) {
+                    World world = player.getWorld();
+                    Location loc = player.getLocation();
+                    loc.setY(loc.getY() - 1);
+                    armorStand = SpawnMob.SpawnArmorStand(event.getPlayer().getLocation(), "20", event.getPlayer(), s, event.getPlayer());
+                    armorStand.setGravity(false);
+
+
+                }
+
                 Location loc = player.getLocation();
-                loc.setY(loc.getY() - 1);
-                armorStand = SpawnMob.SpawnArmorStand(event.getPlayer().getLocation(), "20", event.getPlayer(), s, event.getPlayer());
-                armorStand.setGravity(false);
+                double angle = Math.toRadians(loc.getYaw() + 360);  // Поворачиваем на 360 градусов, чтобы армор-стенд смотрел на игрока
+                double radius = 1.5;  // Радиус вращения
 
+                double x = loc.getX() + radius * Math.sin(angle);
+                double y = loc.getY() + 1;  // Высота армор-стенда относительно игрока
+                double z = loc.getZ() + radius * Math.cos(angle);
 
+                armorStand.teleport(new Location(loc.getWorld(), x, y, z, loc.getYaw(), 0));
+                armorStand.setRotation(loc.getYaw(), 0);
             }
 
-            Location loc = player.getLocation();
-            double angle = Math.toRadians(loc.getYaw() + 360);  // Поворачиваем на 360 градусов, чтобы армор-стенд смотрел на игрока
-            double radius = 1.5;  // Радиус вращения
 
-            double x = loc.getX() + radius * Math.sin(angle);
-            double y = loc.getY() + 1;  // Высота армор-стенда относительно игрока
-            double z = loc.getZ() + radius * Math.cos(angle);
-
-            armorStand.teleport(new Location(loc.getWorld(), x, y, z, loc.getYaw(), 0));
-            armorStand.setRotation(loc.getYaw(), 0);
-
-        }
 
 
 
--- src/main/java/org/super89/supermegamod/learningplugin/LearningPlugin.java
+++ src/main/java/org/super89/supermegamod/learningplugin/LearningPlugin.java
@@ -1,10 +1,13 @@
 package org.super89.supermegamod.learningplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Item;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -19,9 +22,17 @@ public final class LearningPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        ItemStack i = ItemUtils.CreatePlayerHead("Egg");
+        ItemStack i = ItemUtils.CreatePlayerHead("Super89");
         ItemMeta meta = i.getItemMeta();
         meta.setDisplayName(ChatColor.GOLD + "Открыть яйцо");
+        i.setItemMeta(meta);
+        ShapedRecipe recipe = new ShapedRecipe(i);
+        recipe.shape("DOD","GSG","DOD");
+        recipe.setIngredient('D', Material.DIAMOND);
+        recipe.setIngredient('G', Material.GOLD_INGOT);
+        recipe.setIngredient('S',Material.SPONGE);
+        recipe.setIngredient('O', Material.OBSIDIAN);
+        Bukkit.addRecipe(recipe);
 
 
         getServer().getPluginCommand("player_spawn").setExecutor(new SpawnMob(this));
